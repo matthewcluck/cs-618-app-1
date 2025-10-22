3a8b5abbdc34e9d59aadbd50653b1c202827ae0c
Matthew Cluck <53344005+matthewcluck@users.noreply.github.com>
2025-10-22 15:04:59 +0000

    your commit message here

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
-import bodyParser from 'body-parser'
-import cors from 'cors'
 
 const app = express()
 app.use(bodyParser.json())
@@ -11,8 +13,24 @@ app.use(cors())
 postsRoutes(app)
 userRoutes(app)
 
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('user connected:', socket.id)
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id)
+  })
+})
+
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
 })
 
-export { app }
+// export { app }
+
+export { server as app }
